ad26b6f2cb41ec98d1cd66de805c2ac17e7fde54
davidroardev <david_roa_06@hotmail.com>
2024-09-14 22:52:55 -0500

    Funcion de registro añadida y fucional

--- script_BACKEND/login.js
+++ script_BACKEND/login.js
@@ -1,6 +1,9 @@
 window.onload = (event) =>{
     const loginForm = document.getElementById('loginForm');
     const loginMessage = document.getElementById('loginMessage');
+    const registerForm = document.getElementById('registerForm');
+    const registerMessage = document.getElementById('registerMessage')
+
 
     loginForm.addEventListener('submit',  async function(event){
         event.preventDefault();
@@ -33,4 +36,41 @@ window.onload = (event) =>{
         }
     });
 
-}
+    registerForm.addEventListener('submit', async(event)=>{ //esta linea se usa como arrow function para prueba 
+        event.preventDefault();
+
+        const newUserName = document.getElementById('new_username').value;
+        const newPassword = document.getElementById('new_password').value;
+        const email = document.getElementById('email').value;
+        const firstName = document.getElementById('FirstName').value;
+        const lastName = document.getElementById('LastName').value;
+        const phoneNumber = document.getElementById('Phone').value;
+
+        try {
+            const response = await fetch('http://localhost:3000/user/register',{
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({userName:newUserName,password:newPassword,email:email,
+                    firstName:firstName,PhoneNumber:phoneNumber,lastName:lastName})
+            });
+            const data = await response.json();
+            console.log(data);
+            if(response.ok){
+                registerMessage.textContent= 'Registro Exitoso'
+                registerMessage.style.color = '#CCC'
+            }else{
+                registerMessage.textContent= data.message || 'Hubo un Error en el Registro'
+                registerMessage.style.color = '#eb5e28'
+            }
+
+        } catch (error) {
+            console.log(error)
+            registerMessage.textContent = 'Hubo un Error en el Registro catch'
+            registerMessage.style.color = 'red'
+        }
+
+    })
+
+};
